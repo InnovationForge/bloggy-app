9994479486cb18a6dcf94e5c4ed2ac2108351338
Niranjan Rath <niranjan.rath@gmail.com>
2023-05-28 22:55:04 +0200

    guard implementation

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  console.log("here")
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+  if(authService.isAuthenticated()){
+    console.log("User is authenticated")
+    return true;
+  } else {
+    console.log("User is not authenticated")
+    return router.navigate(['login']);
+  }
 };
